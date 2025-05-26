381c386f79dc87bdcd9d266101d997e3c4366c2d
sabna <sabnavv32@gmail.com>
2025-05-26 08:07:38 +0530

    exploring get post, delete

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
 const express= require('express')
 const app= express();
+app.use('/test/2',(req,res)=>{
+    res.send('testing sabna')
+})
+
+app.get('/user',(req,res)=>{
+res.send({firstName:'sabna',lastName:'v v'})
+})
+app.post('/user',(req,res)=>{
+    res.send('data save to db')
+})
+app.delete('/user',(req,res)=>{
+    res.send('data deleted successfully')
+})
 
 app.use('/test',(req,res)=>{
     res.send('heloo from the user')
